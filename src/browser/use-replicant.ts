0dee7e5b9686fe95af5c46d9d7441d09b8fbcd57
secchanu <secchanu929@gmail.com>
2024-07-16 01:41:59 +0900

    fix useRep

--- src/browser/use-replicant.ts
+++ src/browser/use-replicant.ts
@@ -3,7 +3,13 @@ import { Jsonify } from "type-fest";
 
 import { ReplicantMap } from "../nodecg/replicants";
 
+/**
+ * useReplicantを型安全に使うためのラッパー関数
+ */
 export const useRep = <TRepName extends keyof ReplicantMap>(
-  replicantName: string,
+  replicantName: TRepName,
   options: UseReplicantOptions<Jsonify<ReplicantMap[TRepName]>> = {},
-) => useReplicant<ReplicantMap[TRepName]>(replicantName, options);
+): readonly [
+  Jsonify<ReplicantMap[TRepName]> | undefined,
+  (newValue: Jsonify<ReplicantMap[TRepName]>) => void,
+] => useReplicant<ReplicantMap[TRepName]>(replicantName, options);
